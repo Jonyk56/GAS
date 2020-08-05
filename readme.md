a5ad01006e25426956daf43a7ae4e3ffe5cabdf4
Jonyk56 <LestusJ@gmail.com>
2020-08-05 08:15:51 +0000

    reee

---
+++ readme.md
@@ -0,0 +1,29 @@
+## 🎨 Sample Plugin
+Sample plugin for [Graviton Editor](https://github.com/Graviton-Code-Editor/Graviton-App)
+
+### 📜 Usage
+
+Clone the repository in your .graviton2/plugins:
+```shell
+git clone https://github.com/Graviton-Code-Editor/sample-plugin.git
+```
+
+Go inside it:
+```shell
+cd sample-plugin
+```
+
+Install the dependencies:
+```shell
+npm install
+```
+
+Run for development:
+```shell
+npm run watch
+```
+
+To make a build (optional):
+```shell
+npm run build
+```
---
+++ src/index.js
@@ -0,0 +1,46 @@
+export const entry = ({ Notification, RunningConfig, Menu, StaticConfig }) => {
+    RunningConfig.on('aTabHasBeenCreated', ({ client, instance, directory, tabElement }) => {
+        client.do('onChanged', {
+            instance,
+            action() {
+                if (StaticConfig.data.GAS == true) {
+                    RunningConfig.emit('command.saveCurrentFile')
+                }
+            } 
+        })
+    });
+    new Menu({
+        button: 'GAS',
+        list: [
+            {
+                label: 'Enable/Disable',
+                action() {
+                    switch (StaticConfig.data.GAS) {
+                        case true:
+                            StaticConfig.data.GAS = false;
+                            new Notification({
+                                title: "GAS",
+                                content: "AutoSave Disabled!"
+                            })
+                            break;
+                        case false:
+                            StaticConfig.data.GAS = true;
+                            new Notification({
+                                title: "GAS",
+                                content: "AutoSave Enabled!"
+                            })
+                            break;
+                        default:
+                            StaticConfig.data.GAS = true;
+                            new Notification({
+                                title: "GAS",
+                                content: "AutoSave Enabled!"
+                            })
+
+                    }
+                    StaticConfig.triggerChange();
+                }
+            }
+        ]
+    })
+}
